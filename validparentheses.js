a37a463aa358734cbdc09924c412d76db9d6726d
kevindai777 <kevin.dai.2451@gmail.com>
2020-06-22 07:38:55 -0400

    edits to explanation

--- validparentheses.js
+++ validparentheses.js
@@ -1,13 +1,13 @@
 //Objective is to tell whether a set of parentheses is valid.
-//For example... () is valid but ()) is not valid.
+//For example... '()' is valid but '())' is not valid.
 //Parentheses include (), [], and {}
 
 let parentheses = '()'
 
 
-
 //O(n) solution using a stack
 
+//Map out all closing and opening brackets
 let map = {
     ']': '[',
     ')': '(',
@@ -17,20 +17,23 @@ let map = {
 let stack = []
 
 //Check each element...
-//If the element is an opening bracket, push it in
-//Otherwise, if it's a closing bracket, check if the top of the stack has the corresponding opening bracket
-//Pop the opening bracket out since it's been checked and move on
-//If it doesn't correspond, then it's not valid
-//If the stack still has elements by the end of the counter, it's not valid
 for (let i = 0; i < parentheses.length; i++) {
+    //If it's a closing bracket, check if the top of the stack has the corresponding opening bracket
     if (map[parentheses.charAt(i)]) {
+        //Pop the opening bracket out to check it w/ the hashmap
+        //If it doesn't correspond, then it's not valid
         let topElement = stack.length > 0 ? stack.pop() : '#'
         if (map[parentheses.charAt(i)] !== topElement) {
             return false
         }
+    //Otherwise, if the element is an opening bracket, push it in
     } else {
         stack.push(parentheses.charAt(i))
     }
 }
 
+
+//If the stack still has elements by the end of the counter, 
+//That means that some elements haven't been accounted for
+//Therefore, it's not valid
 return stack.length === 0
