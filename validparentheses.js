560f18912330f9404a159435b549f62fe44afb54
Kevin Dai <66235906+kevindai777@users.noreply.github.com>
2020-08-26 23:26:58 -0400

    Updates to everything

--- validparentheses.js
+++ validparentheses.js
@@ -17,25 +17,24 @@ let map = {
 
 let stack = []
 
-//Check each element...
 for (let i = 0; i < parentheses.length; i++) {
     
     //If it's a closing bracket, check if the top of the stack has the corresponding opening bracket
-    if (map[parentheses.charAt(i)]) {
+    if (map[parentheses[i]]) {
         //Pop the opening bracket out to check it w/ the hashmap
         let topElement = stack.length > 0 ? stack.pop() : '#'
         //If it doesn't correspond, then it's not valid
-        if (map[parentheses.charAt(i)] !== topElement) {
+        if (map[parentheses[i]] !== topElement) {
             return false
         }
         
     //Otherwise, if the element is an opening bracket, push it in
     } else {
-        stack.push(parentheses.charAt(i))
+        stack.push(parentheses[i])
     }
 }
 
 //If the stack still has elements by the end of the counter, 
 //that means that some elements haven't been accounted for
 //Therefore, it's not valid
-return stack.length === 0
+return stack.length == 0
