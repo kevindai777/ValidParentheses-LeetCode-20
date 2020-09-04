2c11d9c282410a33c6d305a7f16e40f7ffeec03e
Kevin Dai <adamgordon@KevinDai.home>
2020-09-04 01:42:03 -0400

    solutions

---
+++ validparentheses.java
@@ -0,0 +1,32 @@
+//Java solution
+
+class Solution {
+    
+    private HashMap<Character, Character> map;
+    
+    public Solution() {
+        this.map = new HashMap<Character, Character>();
+        this.map.put(')', '(');
+        this.map.put('}', '{');
+        this.map.put(']', '[');
+    }
+    
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<Character>();
+        
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (this.map.containsKey(c)) {
+                char topElement = stack.empty() ? '#' : stack.pop();
+                
+                if (topElement != this.map.get(c)) {
+                    return false;
+                }
+            } else {
+                stack.push(c);
+            }
+        }
+        
+        return stack.isEmpty();
+    }
+}
